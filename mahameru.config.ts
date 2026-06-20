15fc942ad57ebe497a74018c3048da77f575eeae
Bintan <siregarbintan@gmail.com>
2026-06-20 21:46:20 +0800

    Push initial project

---
+++ mahameru.config.ts
@@ -0,0 +1,11 @@
+import type { MahameruConfig } from "mahameru";
+
+const config: MahameruConfig = {
+    options: {
+        dev: true,
+        port: 3001,
+        host: 'localhost'
+    }
+}
+
+export default config
