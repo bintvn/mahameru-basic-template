65e33204b0dcf9ebcc3a62fce43592f5c6efe93e
Bintan <siregarbintan@gmail.com>
2026-06-23 15:39:17 +0800

    update

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -2,7 +2,7 @@ import { type RouteHandler, MahameruResponse } from 'mahameru/core'
 
 export const GET: RouteHandler = async (request) => {
     return MahameruResponse.json({
-        framework: 'Custom Node Backend Framework',
-        status: 'Online'
+        success: true,
+        message: 'Welcome to MahameruJS!'
     });
 };
